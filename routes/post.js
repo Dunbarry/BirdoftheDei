e8e1aaf4fcd3b6a34830aa215df291acec8ed5f2
Dunbarry <RyanADunbar@gmail.com>
2016-09-12 14:42:07 -0600

    :sparkle: Post page online

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bird of the Dei' });
-});
-
 /* GET post page. */
 router.get('/post', function(req, res, next) {
   res.render('post', { title: 'Bird of the Dei' });
