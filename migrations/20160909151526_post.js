87297780e1966b2f605049516622be16454673df
Dunbarry <RyanADunbar@gmail.com>
2016-09-12 16:12:50 -0600

    :wip: initial post page set up. Now comes the fun par.

--- migrations/20160909151526_post.js
+++ migrations/20160909151526_post.js
@@ -2,9 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("posts", function (table){
     table.increments();
-    table.text('title')    table.text('content')
+    table.text('username')
+    table.text('title')
+    table.text('content')
     table.text('timeStamp')
-    table.text('userId')
   })
 };
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('Drone online.')
+
+// router.post('/add', function (req, res) {
+//   return knex('users')
+//     .insert(req.body)
+//     .then(function () {
+//       res.send('welcome') // or something
+//     })
+// })
