adfd3969d489e40ca71840480843a8489ec6eb2f
Dunbarry <RyanADunbar@gmail.com>
2016-09-21 08:48:02 -0600

    white-space property

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
+console.log(process.env.DATABASE_URL)
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/birdofthedei'
   },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
